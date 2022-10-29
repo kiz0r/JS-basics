b9aef362136a84730a48a78f90486df4e56eaf4f
Kirill Khomenko <kkhomenko10@gmail.com>
2022-10-29 19:35:37 +0200

    make the fourth part of the homework

--- js/main.js
+++ js/main.js
@@ -34,3 +34,29 @@ console.log("num /= 11 =>", (num /= 11)); // num = num  /11;
 console.log("--num =>", ++num); // num = num + 1;
 console.log("--num =>", --num); // num = num – 1;
 /* В предыдущих двух строках используется не постинкремент по той причине, что условие постинкремента выполнится после console.log и не покажет результат в консоль*/
+
+/* part 4 */
+let userInput1, userInput2;
+userInput1 = Number(prompt("Enter the number to move it to the square :"));
+// userInput1 ^ 2
+console.log("The result of squaring =", Math.pow(userInput1, 2));
+
+alert("Input data for arithmetic mean search");
+userInput1 = Number(prompt("Enter the first number :"));
+userInput2 = Number(prompt("Enter the second number :"));
+// (userInput1 + userInput2) / 2
+console.log(
+  "Arithmetic mean of the numbers entered =",
+  (userInput1 + userInput2) / 2
+);
+
+userInput1 = Number(
+  prompt(
+    "Enter the number of minutes and we will transfer it to You in seconds :"
+  )
+);
+console.log("Time in seconds =", userInput1 * 60, "seconds");
+
+let greeting = "Hello,";
+let userName = prompt("Input Your name");
+console.log(greeting + " " + userName);
