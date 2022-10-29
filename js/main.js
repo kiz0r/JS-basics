eadbec5a3e2c67b3918550a073e75ac21df276f0
Kirill Khomenko <kkhomenko10@gmail.com>
2022-10-29 19:07:30 +0200

    make the first part of the homework

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* part 1*/
+
+// a * b
+const a = 25;
+const b = 5;
+console.log("a + b =", a + b); // = 125
+
+// c / d
+const c = 15;
+const d = 5;
+console.log("c / d =", c / d); // = 3
+
+// e + f
+const e = 10;
+const f = 32;
+console.log("e + f =", e + f); // = 42
